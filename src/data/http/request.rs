0fdcdcc014435dc3ce903e495158d287d20ecfca
Ados <aden.huen@gmail.com>
2022-11-17 19:06:35 +0800

    fix

--- src/data/http/request.rs
+++ src/data/http/request.rs
@@ -33,11 +33,6 @@ pub struct Header {
     pub path: Option<String>,
     pub protocol: Option<String>,
     pub headers: Option<HashMap<String, String>>,
-    // pub host: Option<String>,
-    // pub user_agent: Option<String>,
-    // pub accept: Option<String>,
-    // pub content_type: Option<String>,
-    // pub content_length: Option<String>
 }
 
 impl fmt::Debug for Header {
@@ -47,11 +42,6 @@ impl fmt::Debug for Header {
             .field("path", &self.path)
             .field("protocol", &self.protocol)
             .field("headers", &self.headers)
-            //  .field("host", &self.host)
-            //  .field("user_agent", &self.user_agent)
-            //  .field("accept", &self.accept)
-            //  .field("content_type", &self.content_type)
-            //  .field("content_length", &self.content_length)
             .finish()
     }
 }
